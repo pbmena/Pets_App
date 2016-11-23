4f05ea39d961904efd583962fe45206f8b514dad
pablomenaordenes <pb.mena3012@gmail.com>
2016-11-23 17:06:37 -0300

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.android.pets;
+
+/**
+ * Created by pablomenaordenes on 23-11-16.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_pets);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, CatalogActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1600);
+    }
+}
